b6e7a394549e311a31e5fd7e87043b4adcba5a02
j-lorente <jlorentemonzo@gmail.com>
2016-02-18 15:30:29 +0100

    new plot

--- plot.py
+++ plot.py
@@ -1,11 +1,11 @@
 # -*- coding: utf-8 -*-
 """
-Created on Thu Feb  4 12:04:20 2016
 
 @author: Juan Lorente
 MASTERS' THESIS
 
-### This is for plotting the PID ouput of the main program ###
+This is for plotting data from the main program
+
 """
 
 import matplotlib.pyplot as plt
@@ -20,6 +20,7 @@ input_port = yarp.Port()
 input_port.open("/reader")
 yarp.Network.connect("/sender", "/reader")
 
+#Get initial time
 t0 = time.time()
 
 while True:
@@ -28,17 +29,17 @@ while True:
         output = yarp.Bottle()
         input_port.read(output)
         ZMP=output.get(0).asDouble()
-        setpoint=output.get(1).asDouble()
+        #setpoint=output.get(1).asDouble()
         
+        #Get actual time
         t = time.time() - t0
         
-        #Graph 1
-        plt.plot([t], [ZMP],'bo', [t], [setpoint],'ro')
-        plt.axis([0, 50, -20, 20])
+        #Plot graph
+        #plt.plot([t], [ZMP],'bo', [t], [setpoint],'ro')
+        plt.plot([t], [ZMP],'ro')
+        plt.axis([0, 50, -5, 5])
         plt.xlabel('Time (s)')
         plt.ylabel('ZMP (cm)')
-        
-        #Plot graphs
         plt.show()
         
         plt.pause(0.1)
